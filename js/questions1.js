9a2a305498aa1e0ba4f5e26702ad9c6960ac891d
divyabaghel <divyabaghel600@gmail.com>
2021-08-22 17:06:24 +0530

    commit

---
+++ js/questions1.js
@@ -0,0 +1,74 @@
+// creating an array and passing the number, questions, options, and answers
+
+
+let questions = [
+    {
+    numb: 1,
+    question: "The property in CSS used to change the background color of an element is",
+    answer: "background-color",
+    options: [
+        "bgcolor",
+        "color",
+        "background-color",
+        "All of the above",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The CSS property used to control the element's font-size is -",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Are the negative values allowed in padding property?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Can't say",
+      "May be",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following CSS property is used to represent the overflowed text which is not visible to the user?",
+    answer: "text-overflow",
+    options: [
+      "text-shadow",
+      "text-stroke",
+      "text-overflow",
+      "text-decoration",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/questions2.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following statements is/are correct about Bootstrap 4?",
+    answer: "All of the above",
+    options: [
+        "It provides a free front - end framework",
+        "It is a completely open - source product",
+        "It gives the ability to create responsive designs",
+        "All of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is/are the developers of Bootstrap?",
+    answer: "Mark Otto and Jacob Thornton",
+    options: [
+      "Mark Otto and Jacob Thornton",
+      "Tim Berners - Lee",
+      "Håkon Wium Lie",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following contextual class is used for the purpose of showing warning?",
+    answer: ".danger",
+    options: [
+      ".active",
+      ".danger",
+      ".warning",
+      "None of the above",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following class indicates a warning that might need attention?",
+    answer: ".warning",
+    options: [
+        ".active",
+        ".danger",
+        ".warning",
+        "None of the above",
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many columns are allowed in a bootstrap grid system?",
+    answer: "12",
+    options: [
+      "2",
+      "12",
+      "3",
+      "5",
+      
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
